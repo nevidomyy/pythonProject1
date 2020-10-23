5091af02b1fc92d23100604ed562b0525780705d
Igor <nevidom4ik@list.ru>
2020-10-23 23:29:23 +0300

    Редактирование строки, обрезать до определенного символа и тд

--- Learn1.py
+++ Learn1.py
@@ -1,7 +1,20 @@
+#!/usr/bin/env python3
 import sys          #Грузим библиотечный модуль
+import math         #Математические вычисления
+import random       #Генератор случайных чисел
 print('Операционная система: ' + sys.platform)
 print(2 ** 100)     #Возвести 2 в степень 100
 x = 'Spam!'
 print(x * 8)
-
+y = (2 ** 1000)
+print(len(str(y)))  #Вывести Длину(преобразовать в строку число Y)
+S = 'Игорь'
+print(len(S))       #Вывести длину строки Игорь
+print(math.pi)      #Получить число пи
+print(random.randint(1,100))    # Вывести случайно число в диапазоне от 1 до 100
+St = 'Igor3232.exe'
+if St.find('.')!=-1:
+    print(St[: St.find('.')])   #Вывести все до точки
+else:
+    print(St)
 
